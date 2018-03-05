a403d0f777186473d0ec37b7571e0aec836be015
Samuel Lord <sllord@icloud.com>
2018-03-05 14:31:19 -0600

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -1,7 +1,7 @@
 sudo apt install --no-install-recommends -y i3 compton git xfce4-panel
 git clone https://github.com/sparksammy/i3configs.git
 cd i3configs
-cp i3 -r /etc/i3/
+sudo cp i3 -r /etc/i3/
 cp i3 -r ~/.config/i3
 cp compton -r ~/.config/compton
 sudo cp -r wallpapers /wallpapers
