af6a6d66975b84b4271a1a1df805a5a0b8ce926b
Samuel Lord <sllord@icloud.com>
2018-03-07 20:09:40 -0600

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -1,8 +1,10 @@
-sudo apt install --no-install-recommends -y i3 compton git xfce4-panel
+sudo apt remove pcmanfm
+sudo apt install --no-install-recommends -y i3 compton git xfce4-panel nitrogen thunar
 git clone https://github.com/sparksammy/i3configs.git
 cd i3configs
 sudo cp i3 -r /etc/i3/
 cp i3 -r ~/.config/i3
 cp compton -r ~/.config/compton
+sudo mkdir /wallpapers
 sudo cp -r wallpapers /wallpapers
-echo Done! Please reboot!
+echo Done!
