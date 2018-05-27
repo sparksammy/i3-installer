22dab035ddfc10cdc7a30348c04b5a0f0d7dbcc1
Samuel Lord <sllord@icloud.com>
2018-05-26 21:44:47 -0500

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -5,7 +5,11 @@ git clone https://github.com/sparksammy/i3configs.git
 cd i3configs
 sudo cp i3 -r /etc/i3/
 cp i3 -r ~/.config/i3
+cp gtk-2.0 -r ~/.config/gtk-2.0
+cp gtk-3.0 -r ~/.config/gtk-3.0
+cp menus -r ~/.config/menus
 cp compton -r ~/.config/compton
+cp xfce-perchannel-xml -r ~/.config/xfce-perchannel-xml
 sudo mkdir /wallpapers
 sudo cp -r wallpapers /wallpapers
 echo Done!
