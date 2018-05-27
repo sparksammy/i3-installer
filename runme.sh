f4b124e9d9732734347c99067a62a3a9b50d8afc
Samuel Lord <sllord@icloud.com>
2018-05-26 22:04:13 -0500

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -10,7 +10,7 @@ cp gtk-3.0 -r ~/.config/gtk-3.0
 cp menus -r ~/.config/menus
 cp compton -r ~/.config/compton
 cp xfce-perchannel-xml -r ~/.config/xfce-perchannel-xml
-sudo cp sparksammy-logout -r /bin/sparksammy-logout
+sudo cp sparksammy-logout /bin/sparksammy-logout
 sudo chmod 777  /bin/sparksammy-logout
 sudo mkdir /wallpapers
 sudo cp -r wallpapers /wallpapers
